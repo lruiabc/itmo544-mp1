5f087deabc38951b91b17260ea5dbdf8f69989f9
lruiabc <568047669@qq.om>
2015-11-03 10:46:51 -0600

    added a install-env in gih

---
+++ install-env.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y 
+
+git clone https://github.com/lruiabc/itmo-544-444-mp1.git
+
+curl -sS http://getcomposer.org/installer | php
+
+echo -e "\nSleeping 5 seconds"
+for i in {0..5}; do echo -ne '.';sleep 1;done
+echo "\n"
+
+php composer.phar require aws/aws-sdk-php
+
+echo -e "\nSleeping 5 seconds"
+for i in {0..5}; do echo -ne '.';sleep 1;done
+echo "\n"
+
+chmod 755 vendor
+
+sudo mv ./../itmo-544-444-mp1/vendor /var/www/html
+
+
+echo "hello!" >/tmp/hello.txt
