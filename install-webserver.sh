b5a11efef7050188b57ddae9b02e63f1529e9662
lruiabc <568047669@qq.om>
2015-11-07 17:55:43 -0600

    added a new install-webserver.sh to git.

--- install-webserver.sh
+++ install-webserver.sh
@@ -5,10 +5,10 @@ sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/lruiabc/itmo-544-444-mp1.git
 
-mv ./../itmo-544-444-mp1/images /var/www/html/images
-mv ./../itmo-544-444-mp1/vendor /var/www/html/vendor
+#mv ./../itmo-544-444-mp1/images /var/www/html/images
+#mv ./../itmo-544-444-mp1/vendor /var/www/html/vendor
 mv ./../itmo-544-444-mp1/index.html /var/www/html
-mv ./../itmo-544-444-mp1/*.php /var/www/html
+#mv ./../itmo-544-444-mp1/*.php /var/www/html
 
 
 
