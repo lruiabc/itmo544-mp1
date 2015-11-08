ef05bc2f43d9b9f7e955a82c33d9f8941fc68a9c
lruiabc <568047669@qq.om>
2015-11-07 18:14:35 -0600

    added a install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -9,11 +9,13 @@ git clone https://github.com/lruiabc/itmo-544-444-mp1.git
 #mv ./../itmo-544-444-mp1/vendor /var/www/html/vendor
 mv ./../itmo-544-444-mp1/index.html /var/www/html
 #mv ./../itmo-544-444-mp1/*.php /var/www/html
+curl -sS http://getcomposer.org/installer | php
+php composer.phar require aws/aws-sdk-php
 
+#echo -e "\nSleeping 5 seconds"
+#for i in {0..5}; do echo -ne '.';sleep 1;done
 
-
-#./install-env.sh
-
+sudo mv ./../itmo-544-444-mp1/vendor /var/www/html/vendor
 
 echo "hello!" >/tmp/hello.txt
 
