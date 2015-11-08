555b27ce3fb81419b4d76f570b9c9015b19780b7
lruiabc <568047669@qq.om>
2015-11-08 11:58:15 -0600

    added install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,12 +3,13 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
-git clone https://github.com/lruiabc/itmo-544-444-mp1.git
+git clone https://github.com/lruiabc/itmo544-mp1.git
 
-#mv ./../itmo-544-444-mp1/images /var/www/html/images
-#mv ./../itmo-544-444-mp1/vendor /var/www/html/vendor
-mv ./../itmo-544-444-mp1/index.html /var/www/html
-#mv ./../itmo-544-444-mp1/*.php /var/www/html
+mv ./../itmo544-mp1/images /var/www/html/images
+mv ./../itmo544-mp1/vendor /var/www/html/vendor
+mv ./../itmo544-mp1/index.html /var/www/html
+mv ./../itmo544-mp1/*.php /var/www/html
+
 curl -sS http://getcomposer.org/installer | php
 php composer.phar require aws/aws-sdk-php
 
