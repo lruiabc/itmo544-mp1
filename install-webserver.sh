542eda8b1ca857ab92a5f23af77e2cadd4dc8f27
lruiabc <568047669@qq.com>
2015-11-08 12:04:12 -0600

    Update install-webserver.sh

--- install-webserver.sh
+++ install-webserver.sh
@@ -16,7 +16,7 @@ php composer.phar require aws/aws-sdk-php
 #echo -e "\nSleeping 5 seconds"
 #for i in {0..5}; do echo -ne '.';sleep 1;done
 
-sudo mv ./../itmo-544-444-mp1/vendor /var/www/html/vendor
+sudo mv ./../itmo544-mp1/vendor /var/www/html/vendor
 
 echo "hello!" >/tmp/hello.txt
 
